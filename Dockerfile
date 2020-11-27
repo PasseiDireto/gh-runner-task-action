fc73696a4d35928a19d9a3f61dcb4f8efb1cd470
Vinicius Schettino <schettino.viniciusjunqueira@gmail.com>
2020-11-27 19:36:07 -0300

    add basic run_task params

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3-slim AS builder
+FROM python:3.9-alpine AS builder
 # https://jacobtomlinson.dev/posts/2019/creating-github-actions-in-python/
 ADD requirements/run.txt /app/requirements.txt
 ADD task-params-template.json /app/task-params-template.json
@@ -8,9 +8,9 @@ RUN pip install --target=/app -r requirements.txt
 
 ADD action /app
 
-FROM gcr.io/distroless/python3-debian10
-COPY --from=builder /app /app
+#FROM gcr.io/distroless/python3-debian10:3.9
+#COPY --from=builder /app /app
 WORKDIR /app
 ENV PYTHONPATH /app
 
-CMD ["/app/start.py"]
+CMD ["python", "/app/start.py"]
--- action/start.py
+++ action/start.py
@@ -8,15 +8,33 @@ import os
 import boto3
 from rich.console import Console
 
+
+def get_input(environ):
+    prefix = "INPUT_"
+    return {
+        snake_to_camel(k.removeprefix(prefix)): v
+        for k, v in environ.items()
+        if k.startswith(prefix)
+    }
+
+
+def snake_to_camel(name):
+    u_camel_case = "".join(word.title() for word in name.lower().split("_"))
+    return u_camel_case[0].lower() + u_camel_case[1:]
+
+
 if __name__ == "__main__":
     console = Console()
-    input_data = [var for var in os.environ if var.startswith("INPUT_")]
-    general_data = [var for var in os.environ if not var.startswith("INPUT_")]
-    console.print("general data: ", general_data)
-    console.print("input data: ", input_data)
+    input_data = get_input(os.environ)
+    general_data = {v: os.environ[v] for v in os.environ if not v.startswith("INPUT_")}
     task_params_file = os.environ.get("INPUT_TASK_PARAMS_FILE")
     base_config = json.loads(open("./task-params-template.json").read())
     if task_params_file and os.path.exists(task_params_file):
         base_config.update(json.loads(open(task_params_file).read()))
+    for key, value in input_data.items():
+        if key in base_config:
+            base_config[key] = value
+    base_config["startedBy"] = general_data.get("GITHUB_ACTOR", "unknown")
     console.print(base_config)
-    ecr = boto3.client("ecr")
+    ecr = boto3.client("ecs")
+    ecr.run_task(**base_config)
