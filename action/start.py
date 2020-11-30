0f88a9f84575ba170687096dbf71fd01ff2da197
Vinicius Schettino <schettino.viniciusjunqueira@gmail.com>
2020-11-30 09:53:51 -0300

    add new config params

--- action/start.py
+++ action/start.py
@@ -27,6 +27,8 @@ if __name__ == "__main__":
     console = Console()
     input_data = get_input(os.environ)
     general_data = {v: os.environ[v] for v in os.environ if not v.startswith("INPUT_")}
+    console.print(input_data)
+    console.print(general_data)
     task_params_file = os.environ.get("INPUT_TASK_PARAMS_FILE")
     base_config = json.loads(open("./task-params-template.json").read())
     if task_params_file and os.path.exists(task_params_file):
@@ -34,7 +36,22 @@ if __name__ == "__main__":
     for key, value in input_data.items():
         if key in base_config:
             base_config[key] = value
+    base_config["group"] = f"gh-runner:{general_data['GITHUB_REPOSITORY']}"
     base_config["startedBy"] = general_data.get("GITHUB_ACTOR", "unknown")
+    base_config["overrides"]["containerOverrides"][0]["environment"] = [
+        {"name": "GITHUB_PERSONAL_TOKEN", "value": input_data["githubPat"]},
+        {"name": "GITHUB_OWNER", "value": general_data["GITHUB_REPOSITORY_OWNER"]},
+        {"name": "GITHUB_REPOSITORY", "value": general_data["GITHUB_REPOSITORY"]},
+        {"name": "RUNNER_NAME", "value": general_data["GITHUB_JOB"]},
+    ]
+    base_config["networkConfiguration"]["awsvpcConfiguration"]["subnets"] = input_data[
+        "subnets"
+    ].split(",")
+    if "securityGroups" in input_data:
+        base_config["networkConfiguration"]["awsvpcConfiguration"][
+            "securityGroups"
+        ] = input_data.get("securityGroups").split(",")
     console.print(base_config)
     ecr = boto3.client("ecs")
+    console.print(base_config)
     ecr.run_task(**base_config)
